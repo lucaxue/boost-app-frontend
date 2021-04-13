80d93f979ad5ee35fa54d2467f474f1b2dd01188
Ellie <ellieredmayne@hotmail.com>
2021-04-13 14:18:09 +0100

    Responsiveness improved

--- src/Components/GroupFeed/index.js
+++ src/Components/GroupFeed/index.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Heading, Grid, GridItem, Center, Box } from "@chakra-ui/react";
+import { Heading, Grid, GridItem, Text, Center, Box } from "@chakra-ui/react";
 import EventCard from "../EventCard/index";
 import { getEventsByGroupId } from "../../Libs/httpRequests";
 import { useUserContext } from "Libs/userContext";
@@ -30,27 +30,39 @@ function GroupFeed() {
 
   return (
     <Center>
-      <Grid placeItems="center" minH="60vh" mb="100px" maxW="445" w="full">
+      <Grid
+        mt="15px"
+        placeItems="center"
+        minH="60vh"
+        mb="100px"
+        maxW="445"
+        w="full"
+      >
         <Heading>Group Feed</Heading>
-
-        <GridItem py={5} w="full">
-          <Heading ml={2} size="md" color="black">
-            Attending Events
-          </Heading>
-          {eventsWillAttend.map((event) => (
-            <EventCard {...event} key={event.id} willAttend={true} />
-          ))}
-        </GridItem>
-        <GridItem py={5} w="full">
-          <Heading ml={2} size="md" color="black">
-            Other Events
-          </Heading>
-          <Box w="full">
-            {eventsWillNotAttend.map((event) => (
-              <EventCard {...event} key={event.id} willAttend={false} />
-            ))}
-          </Box>
-        </GridItem>
+        {eventsWillAttend.length !== 0 && eventsWillNotAttend.length !== 0 ? (
+          <>
+            <GridItem py={5} w="full">
+              <Heading ml={2} size="md" color="black">
+                Attending Events
+              </Heading>
+              {eventsWillAttend.map((event) => (
+                <EventCard {...event} key={event.id} willAttend={true} />
+              ))}
+            </GridItem>
+            <GridItem py={5} w="full">
+              <Heading ml={2} size="md" color="black">
+                Other Events
+              </Heading>
+              <Box w="full">
+                {eventsWillNotAttend.map((event) => (
+                  <EventCard {...event} key={event.id} willAttend={false} />
+                ))}
+              </Box>
+            </GridItem>
+          </>
+        ) : (
+          <Text>You have no events</Text>
+        )}
       </Grid>
     </Center>
   );
--- src/Components/NavBar/index.js
+++ src/Components/NavBar/index.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React from "react";
 
-import NavLink from '../NavLink';
-import { Flex } from '@chakra-ui/react';
+import NavLink from "../NavLink";
+import { Flex } from "@chakra-ui/react";
+import { Box } from "@material-ui/core";
 
 function NavBar() {
   return (
@@ -10,20 +11,37 @@ function NavBar() {
       bottom={[0, 0, 0, null]}
       top={[null, null, null, 0]}
       textColor="white"
-      flexDirection={['row', 'row', 'row', 'column']}
+      flexDirection={["row", "row", "row", "column"]}
       justifyContent={[
-        'space-between',
-        'space-between',
-        'space-between',
-        'left',
+        "space-between",
+        "space-between",
+        "space-between",
+        "left",
       ]}
-      w={['100%', '100%', '100%', '10%']}
-      h={['max-content', 'max-content', 'max-content', '100vh']}
+      w={["100%", "100%", "100%", "10%"]}
+      h={["max-content", "max-content", "max-content", "100vh"]}
       bg="#1AC0C6"
     >
-      <NavLink to="/">Profile</NavLink>
-      <NavLink to="/CreateEvent">Create Event</NavLink>
-      <NavLink to="/GroupFeed">Group Feed</NavLink>
+      <NavLink to="/">
+        <Box display={[null, "none", "none", "none"]}>
+          <span className="material-icons">account_circle</span>
+        </Box>
+        <Box display={["none", "inline"]}>Profile</Box>
+      </NavLink>
+
+      <NavLink to="/CreateEvent">
+        <Box display={[null, "none", "none", "none"]}>
+          <span className="material-icons">add_circle_outline</span>
+        </Box>
+        <Box display={["none", "inline"]}>Create Event</Box>
+      </NavLink>
+
+      <NavLink to="/GroupFeed">
+        <Box display={[null, "none", "none", "none"]}>
+          <span className="material-icons">group</span>
+        </Box>
+        <Box display={["none", "inline"]}>Group Feed</Box>
+      </NavLink>
     </Flex>
   );
 }
